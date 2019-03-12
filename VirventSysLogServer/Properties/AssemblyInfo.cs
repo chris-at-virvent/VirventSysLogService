184d8c12014995a10a15de6ef3afadcb210d7873
Chris Larson <chris@virvent.com>
2019-03-12 16:09:39 -0500

    Update to assemblyinfo files

--- VirventSysLogServer/Properties/AssemblyInfo.cs
+++ VirventSysLogServer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SyslogServer")]
+[assembly: AssemblyTitle("Virvent SysLog Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany(".")]
-[assembly: AssemblyProduct("SyslogServer")]
-[assembly: AssemblyCopyright("Copyright © . 2012")]
+[assembly: AssemblyProduct("Virvent SysLog Server")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
